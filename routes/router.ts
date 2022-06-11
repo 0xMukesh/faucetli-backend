6eefb339bce03f73d654ce4a33e709e0764f65e5
Jordan Baron <jordannbaron@gmail.com>
2022-06-10 20:02:47 -0400

    add route for solana

--- routes/router.ts
+++ routes/router.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-import token from '../controllers/token';
-import ping from '../controllers/ping';
+import token from "../controllers/token";
+import ping from "../controllers/ping";
+import solana from "../controllers/solana";
 
-router.get('/ping', ping);
-router.get('/token', token);
+router.get("/ping", ping);
+router.get("/token", token);
+router.get("/solana", solana);
 
 export default router;
